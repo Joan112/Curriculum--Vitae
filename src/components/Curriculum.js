9731b282b79ad16c6cbb1a171092ff17acb9aa49
joan manuel iribe martinez <jm_im@outlook.com>
2020-11-13 19:07:31 -0700

    commit inicial Curriculum Viate

---
+++ src/components/Curriculum.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navar } from './Navar'
+
+
+export const Curriculum = () => {
+    return (
+        <div>
+            <Navar />
+            
+        </div>
+    )
+}
---
+++ src/components/Navar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Navar = () => {
+    return (
+        <div className="navar">
+            {/* <h1>Joan manuel iribe mart</h1> */}
+        </div>
+    )
+}
